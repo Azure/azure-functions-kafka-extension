5869d301b310d456c73449d057d02987535944ed
Priya Ananthasankar <prananth@microsoft.com>
2019-05-08 11:33:53 -0700

    setup.py embellishments

--- binding-library/python/README.md
+++ binding-library/python/README.md
@@ -1,7 +1,27 @@
-# Installation
+# Azure Functions Python Binding for Kafka
+This project provides Kafka binding for Azure Functions written in Python that makes it easy
+to consume Kafka messages from one topic.
+
+# Features
+- Reading from a Kafka topic
+
+# Getting Started
+
+## Installation via PyPI
 pip install azure-functions-kafka-binding
 
-# Usage
+## Installation via Git
+`git clone https://github.com/Azure/azure-functions-kafka-extension`
+
+`cd ./bindings_library/python/`
+
+`python setup.py install`
+
+## Minimum Requirements
+- Python 3.6 and above
+- See setup.py for dependencies
+
+## Usage
 
 ```python
 from azure_functions.kafka import KafkaEvent
--- binding-library/python/setup.py
+++ binding-library/python/setup.py
@@ -1,8 +1,24 @@
+import setuptools
 from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
 
 setup(
     name='azure-functions-kafka-binding',
-    version='1.0.0',
+    version='2.0.0',
     packages=['azure_functions.kafka'],
-    install_requires=['azure-functions-worker']
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    author='Microsoft Corporation',
+    url='https://github.com/Azure/azure-functions-kafka-extension',
+    install_requires=['azure-functions-worker'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ]
 )
