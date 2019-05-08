f8b7332f7af86c3475896eaae1c7ecd0bffacfdc
Priya Ananthasankar <prananth@microsoft.com>
2019-05-08 11:37:16 -0700

    setup.py embellishments

--- binding-library/python/setup.py
+++ binding-library/python/setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name='azure-functions-kafka-binding',
-    version='2.0.0',
+    version='1.0.0',
     packages=['azure_functions.kafka'],
     long_description=long_description,
     long_description_content_type='text/markdown',
