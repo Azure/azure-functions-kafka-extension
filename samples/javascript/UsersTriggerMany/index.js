9bbd81d471fc2f2ad5ea96cbad7c4c9f9ae0f026
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-06-19 04:49:37 -0700

    javascript document and sample update

---
+++ samples/javascript/UsersTriggerMany/index.js
@@ -0,0 +1,10 @@
+var string_decode = require('string_decoder').StringDecoder;
+
+module.exports = async function (context, event) {
+    function print(event) {
+        const dec = new string_decode('utf-8');
+        let event_str = dec.write(event);
+        context.log.info(`JavaScript Kafka trigger function called for message ${event_str}`);
+    }
+    event.map(print);
+};
