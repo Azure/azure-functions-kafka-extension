b7c81cfa93c5883fa306a9cab487a1939713f0c1
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-06-26 22:03:04 -0700

    remove the comment out line

--- samples/container/kafka-function/src/main/java/com/contoso/kafka/TriggerFunction.java
+++ samples/container/kafka-function/src/main/java/com/contoso/kafka/TriggerFunction.java
@@ -12,19 +12,6 @@ import com.microsoft.azure.functions.kafka.*;
  */
 public class TriggerFunction {
     /**
-     * This function consume KafkaEvents on the localhost. Change the topic, brokerList, and consumerGroup to fit your enviornment.
-     * The function is trigged one for each KafkaEvent
-     * @param kafkaEventData
-     * @param context
-     */
-    // @FunctionName("KafkaTrigger-Java")
-    // public void runOne(
-    //         @KafkaTrigger(topic = "my-confluent-topic", brokerList="localhost:31090",consumerGroup="$Default") String kafkaEventData,
-    //         final ExecutionContext context) {
-    //     context.getLogger().info(kafkaEventData);
-    // }
-
-    /**
      * This function consume KafkaEvents on the confluent cloud. Create a local.settings.json or configure AppSettings for configring
      * BrokerList and UserName, and Password. The value wrapped with `%` will be replaced with enviornment variables. 
      * For more details, refer https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-expressions-patterns#binding-expressions---app-settings
