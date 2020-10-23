d6ddb6c48a4ad3e6e765fe0123b76b7f677f0bb2
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-10-23 16:34:59 -0700

    Upgrade 1.5.2 and improve debugging  with avoiding limitation of Azure Platform by introducing new default values

--- samples/dotnet/KafkaFunctionSample/SimpleKafkaTriggers.cs
+++ samples/dotnet/KafkaFunctionSample/SimpleKafkaTriggers.cs
@@ -7,50 +7,87 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.IO;
+using System.Text;
 
 namespace KafkaFunctionSample
 {
     public class SimpleKafkaTriggers
     {
-        [FunctionName(nameof(SampleConsumer))]
-        public void SampleConsumer(
-    [KafkaTrigger(
+        [FunctionName(nameof(ConsoleConsumer))]
+        public void ConsoleConsumer(
+        [KafkaTrigger(
             "LocalBroker",
-            "%EHTOPIC%",
+            "stringTopicTenPartitions",
             ConsumerGroup = "$Default",
-            Username = "$ConnectionString",
-            Password = "%EventHubConnectionString%",
-            Protocol = BrokerProtocol.SaslSsl,
-            AuthenticationMode = BrokerAuthenticationMode.Plain)] KafkaEventData<string> kafkaEvent,
-    ILogger logger)
+            AuthenticationMode = BrokerAuthenticationMode.Plain)] KafkaEventData<string>[] kafkaEvents,
+            ILogger logger)
         {
-            logger.LogInformation(kafkaEvent.Value.ToString());
+            foreach(var kafkaEvent in kafkaEvents)
+                logger.LogInformation(kafkaEvent.Value.ToString());
         }
 
-        [FunctionName(nameof(SampleProducer))]
-        public IActionResult SampleProducer(
+        [FunctionName(nameof(ConsoleProducer))]
+        public static IActionResult ConsoleProducer(
         [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-        [Kafka(
-            "LocalBroker",
-            "%EHTOPIC%",
-            Username = "$ConnectionString",
-            Password = "%EventHubConnectionString%",
-            Protocol = BrokerProtocol.SaslSsl,
-            AuthenticationMode = BrokerAuthenticationMode.Plain)] out KafkaEventData<string>[] kafkaEventData,
-        ILogger logger)
+        [Kafka("LocalBroker", "stringTopicTenPartitions")] out string kafkaEventData,
+        ILogger log)
         {
-            var data = new StreamReader(req.Body).ReadToEnd();
-            kafkaEventData = new[] {
-                    new KafkaEventData<string>()
-                    {
-                        Value = data + ":1:" + DateTime.UtcNow.Ticks,
-                    },
-                    new KafkaEventData<string>()
-                    {
-                        Value = data + ":2:" + DateTime.UtcNow.Ticks,
-                    },
-                };
+            try
+            {
+                var data = new StreamReader(req.Body).ReadToEnd();
+                kafkaEventData = data + ":1:" + DateTime.UtcNow.Ticks;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Are you sure the topic 'stringTopic' exists? To create using Confluent Docker quickstart run this command: 'docker-compose exec broker kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic stringTopicTenPartitions'", ex);
+            }
+
             return new OkResult();
         }
+
+        // EventHubs Configuration sample
+        //
+        //[FunctionName(nameof(SampleConsumer))]
+        //public void SampleConsumer(
+        //[KafkaTrigger(
+        //    "LocalBroker",
+        //    "%EHTOPIC%",
+        //    ConsumerGroup = "$Default",
+        //    Username = "$ConnectionString",
+        //    Password = "%EventHubConnectionString%",
+        //    Protocol = BrokerProtocol.SaslSsl,
+        //    AuthenticationMode = BrokerAuthenticationMode.Plain)] KafkaEventData<string> kafkaEvent,
+        //ILogger logger)
+        //{
+        //    logger.LogInformation(kafkaEvent.Value.ToString());
+        //}
+
+        // EventHubs Configuration sample
+        //
+        //[FunctionName(nameof(SampleProducer))]
+        //public IActionResult SampleProducer(
+        //[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        //[Kafka(
+        //    "LocalBroker",
+        //    "%EHTOPIC%",
+        //    Username = "$ConnectionString",
+        //    Password = "%EventHubConnectionString%",
+        //    Protocol = BrokerProtocol.SaslSsl,
+        //    AuthenticationMode = BrokerAuthenticationMode.Plain)] out KafkaEventData<string>[] kafkaEventData,
+        //ILogger logger)
+        //{
+        //    var data = new StreamReader(req.Body).ReadToEnd();
+        //    kafkaEventData = new[] {
+        //            new KafkaEventData<string>()
+        //            {
+        //                Value = data + ":1:" + DateTime.UtcNow.Ticks,
+        //            },
+        //            new KafkaEventData<string>()
+        //            {
+        //                Value = data + ":2:" + DateTime.UtcNow.Ticks,
+        //            },
+        //        };
+        //    return new OkResult();
+        //}
     }
 }
