906fc751e3e861f01b8905f4394051b4c536ebf8
jainharsh98 <jainh@microsoft.com>
2022-02-24 14:29:39 +0530

    Adding Java Function on local

---
+++ script/run_function.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FUNCTION_DIR=`./test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/server/java8`
+
+cd $FUNCTION_DIR
+
+mvn clean package
+mvn azure-functions:run
---
+++ script/setup_test_pipeline.ps1
@@ -0,0 +1,40 @@
+param
+(
+    [Switch]
+    $UseCoreToolsBuildFromIntegrationTests
+)
+
+$FUNC_RUNTIME_VERSION = '4'
+$arch = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture.ToString().ToLowerInvariant()
+$os = if ($IsWindows) { "win" } else { if ($IsMacOS) { "osx" } else { "linux" } }
+
+$env:CORE_TOOLS_URL = $null
+$coreToolsUrl = $null
+if ($UseCoreToolsBuildFromIntegrationTests.IsPresent)
+{
+    Write-Host "Install the Core Tools for Integration Tests..."
+    $env:CORE_TOOLS_URL = "https://functionsintegclibuilds.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest/Azure.Functions.Cli.$os-$arch.zip"
+    $coreToolsUrl = "https://functionsintegclibuilds.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest"
+}
+else
+{
+    Write-Host "Install the Core Tools..."
+    $env:CORE_TOOLS_URL = "https://functionsclibuilds.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest/Azure.Functions.Cli.$os-$arch.zip"
+    $coreToolsUrl = "https://functionsclibuilds.blob.core.windows.net/builds/$FUNC_RUNTIME_VERSION/latest"
+}
+
+$FUNC_CLI_DIRECTORY = Join-Path $PSScriptRoot 'Azure.Functions.Cli'
+
+Write-Host 'Deleting the Core Tools if exists...'
+Remove-Item -Force "$FUNC_CLI_DIRECTORY.zip" -ErrorAction Ignore
+Remove-Item -Recurse -Force $FUNC_CLI_DIRECTORY -ErrorAction Ignore
+
+$version = Invoke-RestMethod -Uri "$coreToolsUrl/version.txt"
+Write-Host "Downloading the Core Tools (Version: $version)..."
+
+$output = "$FUNC_CLI_DIRECTORY.zip"
+Write-Host "Downloading the Core Tools from url: $env:CORE_TOOLS_URL"
+Invoke-RestMethod -Uri $env:CORE_TOOLS_URL -OutFile $output
+
+Write-Host 'Extracting Core Tools...'
+Expand-Archive $output -DestinationPath $FUNC_CLI_DIRECTORY
