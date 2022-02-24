20cf9947afc61407dda0dec807fa13ef321ef5c6
jainharsh98 <jainh@microsoft.com>
2022-02-24 16:26:56 +0530

    Testing Func core tools

--- script/run_function.sh
+++ script/run_function.sh
@@ -2,9 +2,12 @@
 
 FUNCTION_DIR="./test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/server/java8"
 
+export PATH=$PATH:./Azure.Functions.Cli
+echo PATH
+
+func --version
+
 cd $FUNCTION_DIR
 
-export PATH=$PATH:/Azure.Functions.Cli
-
-mvn clean package
-mvn azure-functions:run
+# mvn clean package
+# mvn azure-functions:run
