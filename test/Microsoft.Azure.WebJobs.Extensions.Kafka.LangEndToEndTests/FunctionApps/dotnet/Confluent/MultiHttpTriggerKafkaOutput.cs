132f503713c495be55baba25851f80802c635473
krishna-kariya <kkariya@microsoft.com>
2022-03-11 17:56:23 +0530

    Updated Multi Kafka Output Binding in dotnet

--- test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/FunctionApps/dotnet/Confluent/MultiHttpTriggerKafkaOutput.cs
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/FunctionApps/dotnet/Confluent/MultiHttpTriggerKafkaOutput.cs
@@ -20,18 +20,23 @@ namespace Confluent
                     Username = "ConfluentCloudUserName",
                     Password = "ConfluentCloudPassword",
                     Protocol = BrokerProtocol.SaslSsl,
-                   AuthenticationMode = BrokerAuthenticationMode.Plain
-            )] out string eventData,
-            ILogger log)
+                    AuthenticationMode = BrokerAuthenticationMode.Plain)] out KafkaEventData<string>[] eventData,
+                    ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string message = req.Query["message"];
+            string message1 = req.Query["message1"];
+            string message2 = req.Query["message2"];
 
             string responseMessage = string.IsNullOrEmpty(message)
                 ? "This HTTP triggered function executed successfully. Pass a message in the query string"
-                : $"Message {message} sent to the broker. This HTTP triggered function executed successfully.";
-            eventData = $"Received message: {message}";
+                : $"Message {message} {message1} {message2} sent to the broker. This HTTP triggered function executed successfully.";
+            eventData = new KafkaEventData<string>[3];
+            eventData[0] = new KafkaEventData<string>(message);
+            eventData[1] = new KafkaEventData<string>(message1);
+            eventData[2] = new KafkaEventData<string>(message2);
+
 
             return new OkObjectResult(responseMessage);
         }
