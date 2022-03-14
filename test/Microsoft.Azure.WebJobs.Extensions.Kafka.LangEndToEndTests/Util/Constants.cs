970c336630acd94ddd51e8283f11ea8942620dfc
Harsh Jain <jainh@microsoft.com>
2022-03-14 21:43:20 +0530

    Adding Constants and BrokerType

---
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/Util/Constants.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests.apps.languages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests.Util
+{
+	public static class Constants
+	{
+		public const int BATCH_MESSAGE_COUNT = 5;
+		public const string DOCKER_RUN = "docker run";
+		public const string DOCKER_PORT_FLAG = "-p";
+		public const string COLON_7071 = ":7071";
+		public const string DOCKER_ENVVAR_FLAG = "-e";
+		public const string CONFLUENT_USERNAME_VAR = "ConfluentCloudUserName";
+		public const string CONFLUENT_PASSWORD_VAR = "ConfluentCloudPassword";
+		public const string CONFLUENT_BROKERLIST_VAR = "ConfluentBrokerList";
+		public const string EVENTHUB_CONSTRING_VAR = "EventhubConnectionString";
+		public const string EVENTHUB_BROKERLIST_VAR = "EventhubBrokerList";
+		public const string SPACE_CHAR = " ";
+		public const string E2E = "e2e";
+		public const string KAFKA = "";
+		public const string PYTHON_SINGLE_APP_NAME = "";
+		public const string FUNC_START = "func start";
+
+		public const string HIPHEN = "-";
+		public const string PYTHONAPP_CONFLUENT_PORT = "";
+		public const string PYTHONAPP_CONFLUENT_IMAGE = "";
+
+		public static Dictionary<Language, string> LanguagePortMapping = new Dictionary<Language, string>()
+		{
+			{Language.PYTHON, PYTHONAPP_CONFLUENT_PORT}
+		};
+		public static Dictionary<Language, string> LanguageImageMapping = new Dictionary<Language, string>()
+		{
+			{ Language.PYTHON, PYTHONAPP_CONFLUENT_IMAGE}
+		};
+	}
+}
---
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/apps/brokers/BrokerType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests.apps.brokers
+{
+    public enum BrokerType
+    {
+        CONFLUENT,
+        EVENTHUB
+    }
+}
