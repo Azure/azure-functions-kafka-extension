32c3fcfab07fcb9c5feba34c9e526f40b11a6d8b
krishna-kariya <kkariya@microsoft.com>
2022-03-14 15:20:41 +0530

    Created Function App for python runtime

---
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/FunctionApps/python/EventHub/MultiKafkaTriggerQueueOutput/__init__.py
@@ -0,0 +1,13 @@
+import logging
+from azure.functions import KafkaEvent, Out
+import typing
+import json 
+
+def main(kevents : typing.List[KafkaEvent], msg: Out[typing.List[str]]):
+    messages = []
+    logging.info(kevents)
+    for kevent in kevents:
+        event = kevent.get_body().decode('utf-8')
+        logging.info(kevent.metadata)
+        messages.append(json.loads(event)['Value'])
+    queueMsg.set(messages)
---
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/FunctionApps/python/EventHub/SingleKafkaTriggerQueueOutput/__init__.py
@@ -0,0 +1,7 @@
+import logging
+from azure.functions import KafkaEvent, Out
+
+def main(kevent : KafkaEvent, msg: Out[str]):
+    logging.info(kevent.get_body().decode('utf-8'))
+    logging.info(kevent.metadata)
+    queueMsg.set(kevent.metadata['Value'])
