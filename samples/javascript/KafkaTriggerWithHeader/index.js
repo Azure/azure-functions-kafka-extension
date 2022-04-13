7dbde41cc4400b391ad7bf9dc7bb57de66ad1d86
krishna-kariya <kkariya@microsoft.com>
2022-04-13 14:57:26 +0530

    Added samples for Javascript runtime

---
+++ samples/javascript/KafkaTriggerWithHeader/index.js
@@ -0,0 +1,9 @@
+var string_decode = require('string_decoder').StringDecoder;
+
+module.exports = async function (context, event) {
+    // const dec = new string_decode('utf-8');
+    // let event_str = dec.write(event);
+    let event_str = event;
+    context.log.info(`JavaScript Kafka trigger function called for message ${JSON.stringify(event_str)}`);
+    console.log("Headers ",context.bindingData.headers);
+};
