5f413b6ec366c10e8b81d9a4ea75df5ec7929b27
Harsh Jain <jainh@microsoft.com>
2022-05-19 17:21:50 +0530

    Uncommenting old tests and removing managedDependency for powershell functions

--- test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/KafkaLanguageEndToEndTests.cs
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/KafkaLanguageEndToEndTests.cs
@@ -16,38 +16,38 @@ namespace Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests
             this.endToEndTestFixture = endToEndTestFixture;
         }
 
-        //[Fact]
-        //public async Task Java8_Smoke_Test_For_Output_And_SingleTrigger()
-        //{
-        //    var random = new Random();
-        //    string inputMessage = $"hello.{random.Next(0, 9999)}";
+		[Fact]
+		public async Task Java8_Smoke_Test_For_Output_And_SingleTrigger()
+		{
+			var random = new Random();
+			string inputMessage = $"hello.{random.Next(0, 9999)}";
 
-        //    var consumer = endToEndTestFixture.ConsumerFactory("java8");
-        //    consumer.Subscribe("java8result");
+			var consumer = endToEndTestFixture.ConsumerFactory("java8");
+			consumer.Subscribe("java8result");
 
-        //    var response = await endToEndTestFixture.HttpClient.GetAsync($"http://localhost:7071/api/HttpTriggerAndKafkaOutput?message={inputMessage}");
-        //    Assert.True(response.IsSuccessStatusCode);
+			var response = await endToEndTestFixture.HttpClient.GetAsync($"http://localhost:7071/api/HttpTriggerAndKafkaOutput?message={inputMessage}");
+			Assert.True(response.IsSuccessStatusCode);
 
-        //    var result = consumer.Consume(10 * 1000);
+			var result = consumer.Consume(10 * 1000);
 
 
-        //    Assert.Equal(inputMessage, result.Message.Value);
-        //}
-            
+			Assert.Equal(inputMessage, result.Message.Value);
+		}
 
-        //[Fact]
-        //public async Task Python38_Smoke_Test_For_Output_And_SingleTrigger()
-        //{
-        //    string inputMessage = $"hello:{DateTime.UtcNow}";
-        //    var consumer = endToEndTestFixture.ConsumerFactory("python38");
-        //    consumer.Subscribe("python38result");
 
-        //    var response = await endToEndTestFixture.HttpClient.PostAsync($"http://localhost:7072/api/kafka_output", new StringContent(inputMessage));
-        //    Assert.True(response.IsSuccessStatusCode);
+		[Fact]
+		public async Task Python38_Smoke_Test_For_Output_And_SingleTrigger()
+		{
+			string inputMessage = $"hello:{DateTime.UtcNow}";
+			var consumer = endToEndTestFixture.ConsumerFactory("python38");
+			consumer.Subscribe("python38result");
 
-        //    var result = consumer.Consume(10 * 1000);
-        //    Assert.Equal(inputMessage, result.Message.Value);
-        //}
+			var response = await endToEndTestFixture.HttpClient.PostAsync($"http://localhost:7072/api/kafka_output", new StringContent(inputMessage));
+			Assert.True(response.IsSuccessStatusCode);
 
-    }
+			var result = consumer.Consume(10 * 1000);
+			Assert.Equal(inputMessage, result.Message.Value);
+		}
+
+	}
 }
