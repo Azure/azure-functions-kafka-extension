af911a111049040ffc4c54aef131cb9e1764639f
Harsh Jain <jainh@microsoft.com>
2022-05-25 12:38:02 +0530

    Adding comments to powershell confluent apps

--- test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/FunctionApps/powershell/Confluent/MultiKafkaTriggerQueueOutput/run.ps1
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/FunctionApps/powershell/Confluent/MultiKafkaTriggerQueueOutput/run.ps1
@@ -3,12 +3,16 @@ using namespace System.Net
 param($kafkaEvents, $TriggerMetadata)
 
 $messages = @()
+
+#Iterating over batch kafka events
 foreach ($kafkaEvent in $kafkaEvents) {
     $kafkaEvent | ConvertFrom-Json -AsHashtable -outvariable event
+    #Adding kafka events to messages
     $messages += $event.Value
 }
 
 $TriggerMetadata
 $messages
 
+#Pushing output binding message
 Push-OutputBinding -Name Msg -Value $messages
--- test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/FunctionApps/powershell/Confluent/SingleKafkaTriggerQueueOutput/run.ps1
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/FunctionApps/powershell/Confluent/SingleKafkaTriggerQueueOutput/run.ps1
@@ -2,12 +2,15 @@ using namespace System.Net
 
 param($kafkaEvent, $TriggerMetadata)
 
+#Kafka Event received from trigger binding
 $kafkaEvent 
 
 $TriggerMetadata
 
+#Queue output to be sent with output binding
 $message = $kafkaEvent.Value
 
 $message
 
+#Pushing output binding message
 Push-OutputBinding -Name Msg -Value $message
