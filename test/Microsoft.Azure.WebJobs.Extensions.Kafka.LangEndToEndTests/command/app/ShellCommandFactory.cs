36e516931ba900d3b0de7958be43c4fd94a9cf3d
Harsh Jain <jainh@microsoft.com>
2022-07-21 14:34:58 +0530

    Adding License Header

--- test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/command/app/ShellCommandFactory.cs
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests/command/app/ShellCommandFactory.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests.apps.brokers;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests.apps.brokers;
 using Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests.apps.languages;
 using Microsoft.Azure.WebJobs.Extensions.Kafka.LangEndToEndTests.Util;
 using System;
