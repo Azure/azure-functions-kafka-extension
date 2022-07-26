cc0b43e11fbc6cf85fe36b8fb6de5fa9d32405c3
shrohilla <shrohilla@microsoft.com>
2022-07-26 13:36:59 +0530

    adding the samples for dotnet/java/python/powershell/javascript/typescript

---
+++ samples/javascript/KafkaTriggerRetryExponential/index.js
@@ -0,0 +1,5 @@
+module.exports = async function (context, event) {
+    // context.log.info(event)
+    context.log.info(`JavaScript Kafka trigger function called for message ${event.Value}`);
+    throw "Unhandled Error"
+};
