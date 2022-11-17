413579e61bf83060218fae72cc1a70715dd7cc91
shrohilla <shrohilla@microsoft.com>
2022-11-17 11:48:41 +0530

    refactoring the code and removing uneccessary comments

--- src/Microsoft.Azure.WebJobs.Extensions.Kafka/Output/KafkaProducerEntity.cs
+++ src/Microsoft.Azure.WebJobs.Extensions.Kafka/Output/KafkaProducerEntity.cs
@@ -32,67 +32,15 @@ namespace Microsoft.Azure.WebJobs.Extensions.Kafka
 
             if (!(item is ICollection))
             {
-                //await kafkaProducer.ProduceAsync(this.Topic, this.GetItemToProduce(item));
                 kafkaProducer.Produce(this.Topic, this.GetItemToProduce(item));
                 kafkaProducer.Flush();
                 return Task.CompletedTask;
             }
-            ICollection collection = (ICollection)item;
-            /*if (EventsOrderType.KEY.Equals(this.Attribute.OrderType))
-            {
-                ProduceKafkaEventsByKey(collection, kafkaProducer);
-            }*/
-
-            //await ProduceEvents(collection, kafkaProducer);
-            //ProduceEvents(collection, kafkaProducer);
-            ProduceEvent(collection, kafkaProducer);
+            ProduceEvents((ICollection)item, kafkaProducer);
             return Task.CompletedTask;
         }
 
-        /*private async void ProduceKafkaEventsByKey(ICollection items, IKafkaProducer kafkaProducer)
-        {
-            IDictionary<object, List<object>> eventMap = BuildKeyDictionary(items);
-            if (eventMap.Count == 0)
-            {
-                return;
-            }
-            List<Task> taskList = new List<Task>();
-            foreach (KeyValuePair<object, List<object>> entry in eventMap)
-            {
-                taskList.Add(ProduceEvents(entry.Value, kafkaProducer));
-            }
-            await Task.WhenAll(taskList);
-        }
-
-        private static IDictionary<object, List<object>> BuildKeyDictionary(ICollection items)
-        {
-            IDictionary<object, List<object>> eventMap = new Dictionary<object, List<object>>();
-            foreach (var item in items)
-            {
-                object key = item.GetType().GetProperty("Key").GetValue(item);
-                List<object> eventDataList = eventMap.TryGetValue(key, out eventDataList) ? eventDataList : new List<object>();
-                eventDataList.Add(item);
-                eventMap[key] = eventDataList;
-            }
-
-            return eventMap;
-        }
-
         private void ProduceEvents(ICollection collection, IKafkaProducer kafkaProducer)
-        {
-            if (collection == null || collection.Count == 0)
-            {
-                return;
-            }
-            if (EventsOrderType.NONE.Equals(this.Attribute.OrderType))
-            {
-                await ProduceEventsAsync(collection, kafkaProducer);
-                return;
-            }
-            ProduceEvent(collection, kafkaProducer);
-        }*/
-
-        private void ProduceEvent(ICollection collection, IKafkaProducer kafkaProducer)
         {
             foreach (var collectionItem in collection)
             {
@@ -100,16 +48,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.Kafka
             }
             kafkaProducer.Flush();
         }
-
-        /*private async Task ProduceEventsAsync(ICollection collection, IKafkaProducer kafkaProducer)
-        {
-            List<Task> tasks = new List<Task>();
-            foreach (var collectionItem in collection)
-            {
-                tasks.Add(kafkaProducer.ProduceAsync(this.Topic, this.GetItemToProduce(collectionItem)));
-            }
-            await Task.WhenAll(tasks);
-        }*/
 
         private object GetItemToProduce<T>(T item)
         {
