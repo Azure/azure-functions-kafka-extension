52f4b957d6efb0bce6db550d1614e6f1d94a023b
Pranava Vedagnya Gaddam <68387945+aloiva@users.noreply.github.com>
2023-06-06 09:44:50 +0530

    fixed duplicate function

--- src/Microsoft.Azure.WebJobs.Extensions.Kafka/Listeners/KafkaTargetScaler.cs
+++ src/Microsoft.Azure.WebJobs.Extensions.Kafka/Listeners/KafkaTargetScaler.cs
@@ -1,12 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Confluent.Kafka;
 using Microsoft.Azure.WebJobs.Host.Scale;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Kafka
