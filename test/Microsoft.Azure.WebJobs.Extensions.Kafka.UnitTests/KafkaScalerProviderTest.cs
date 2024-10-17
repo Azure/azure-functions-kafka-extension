26c13e30b3c0bc7cacf4fa8964724a7421dfd6c7
jainharsh98 <jainh@microsoft.com>
2024-10-17 21:45:43 +0530

    Unit tests and Improved handling for SSL PEM attributes (#520)
    * Adding Trigger Attribute test for ssl
    
    * Adding UTs for Kafka Scaler Provider and code cleanup

---
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.UnitTests/KafkaScalerProviderTest.cs
@@ -0,0 +1,100 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Confluent.Kafka;
+using Microsoft.Azure.WebJobs.Host.Scale;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Kafka.UnitTests
+{
+
+    public class KafkaScalerProviderTest
+    {
+        private readonly Mock<IServiceProvider> serviceProvider;
+        private readonly Mock<INameResolver> nameResolver;
+        private Mock<IConfiguration> config;
+
+        public KafkaScalerProviderTest()
+        {
+            config = new Mock<IConfiguration>(MockBehavior.Loose);
+            config.Setup(p => p.GetSection(It.IsAny<string>())).Returns<IConfigurationSection>(null);
+            nameResolver = new Mock<INameResolver>(MockBehavior.Strict);
+            serviceProvider = new Mock<IServiceProvider>(MockBehavior.Strict);
+            serviceProvider.Setup(p => p.GetService(typeof(INameResolver))).Returns(nameResolver.Object);
+            serviceProvider.Setup(p => p.GetService(typeof(IOptions<KafkaOptions>))).Returns(null);
+            serviceProvider.Setup(p => p.GetService(typeof(IConfiguration))).Returns(config.Object);
+            serviceProvider.Setup(p => p.GetService(typeof(ILoggerFactory))).Returns(new NullLoggerFactory());
+        }
+
+        [Fact]
+        public void kafkaScalerProvider_With_SaslSSl_Metadata()
+        {
+            var metadata = new JObject
+            {
+                { "BrokerList", "brokerList" },
+                { "Topic", "topicTest" },
+                { "ConsumerGroup", "consumerGroup" },
+                { "LagThreshold", 1000 },
+                { "AuthenticationMode", "Plain" },
+                { "Protocol", "SaslSsl" },
+                { "Username", "username" },
+                { "Password", "password" },
+            };
+            var triggerMetadata = new TriggerMetadata(metadata);
+            var kafkaScalerProvider = new KafkaScalerProvider(serviceProvider.Object, triggerMetadata);
+            Assert.NotNull(kafkaScalerProvider);
+            Assert.NotNull(kafkaScalerProvider.GetTargetScaler());
+            Assert.NotNull(kafkaScalerProvider.GetMonitor());
+        }
+
+        [Fact]
+        public void kafkaScalerProvider_With_SSL_Metadata()
+        { 
+            var metadata = new JObject
+            {
+                { "BrokerList", "brokerList" },
+                { "Topic", "topicTest" },
+                { "ConsumerGroup", "consumerGroup" },
+                { "LagThreshold", 1000 },
+                { "Protocol", "Ssl" },
+                { "SslCaPEM", "dummycapem" },
+                { "SslCertificatePEM", "dummycertificatepem" },
+                { "SslKeyPEM", "dummySslKeyPEM"},
+            };
+            var triggerMetadata = new TriggerMetadata(metadata);
+            var kafkaScalerProvider = new KafkaScalerProvider(serviceProvider.Object, triggerMetadata);
+            Assert.NotNull(kafkaScalerProvider);
+            Assert.NotNull(kafkaScalerProvider.GetTargetScaler());
+            Assert.NotNull(kafkaScalerProvider.GetMonitor());
+        }
+
+        [Fact]
+        public void kafkaScaletProvider_With_SSL_Keyvault_Metadata() 
+        { 
+            var metadata = new JObject
+            {
+                { "BrokerList", "brokerList" },
+                { "Topic", "topicTest" },
+                { "ConsumerGroup", "consumerGroup" },
+                { "LagThreshold", 1000 },
+                { "Protocol", "Ssl" },
+                { "SslCaPEM", "dummycapem" },
+                { "SslCertificateandKeyPEM", "dummycertificateandkeypem" }
+            };
+            var triggerMetadata = new TriggerMetadata(metadata);
+            var kafkaScalerProvider = new KafkaScalerProvider(serviceProvider.Object, triggerMetadata);
+            Assert.NotNull(kafkaScalerProvider);
+            Assert.NotNull(kafkaScalerProvider.GetTargetScaler());
+            Assert.NotNull(kafkaScalerProvider.GetMonitor());
+        }
+    }
+}
