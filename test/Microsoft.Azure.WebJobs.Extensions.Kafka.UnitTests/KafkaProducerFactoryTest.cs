487c3c2947d6e80eaf3c7ea92bd7825a98d12101
Pranava Vedagnya Gaddam <prgaddam@microsoft.com>
2025-07-21 15:38:43 +0530

    Revert "remove schema registry UTs"
    This reverts commit b6c46db7e55fd474b1afe246ba481a1b053009a4.

--- test/Microsoft.Azure.WebJobs.Extensions.Kafka.UnitTests/KafkaProducerFactoryTest.cs
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.UnitTests/KafkaProducerFactoryTest.cs
@@ -126,28 +126,28 @@ namespace Microsoft.Azure.WebJobs.Extensions.Kafka.UnitTests
             }
         }
 
-        //[Fact]
-        //public void When_Schema_Registry_Is_Provided_Should_Create_GenericRecord_Listener()
-        //{
-        //    var attribute = new KafkaAttribute("brokers:9092", "myTopic");
+        [Fact]
+        public void When_Schema_Registry_Is_Provided_Should_Create_GenericRecord_Listener()
+        {
+            var attribute = new KafkaAttribute("brokers:9092", "myTopic");
 
-        //    attribute.SchemaRegistryUrl = "localhost";
+            attribute.SchemaRegistryUrl = "localhost";
 
-        //    var entity = new KafkaProducerEntity()
-        //    {
-        //        Attribute = attribute,
-        //        ValueType = typeof(GenericRecord),
-        //    };
+            var entity = new KafkaProducerEntity()
+            {
+                Attribute = attribute,
+                ValueType = typeof(GenericRecord),
+            };
 
-        //    var factory = new KafkaProducerFactory(emptyConfiguration, new DefaultNameResolver(emptyConfiguration), NullLoggerFactory.Instance);
-        //    var producer = factory.Create(entity);
+            var factory = new KafkaProducerFactory(emptyConfiguration, new DefaultNameResolver(emptyConfiguration), NullLoggerFactory.Instance);
+            var producer = factory.Create(entity);
 
-        //    Assert.NotNull(producer);
-        //    Assert.IsType<KafkaProducer<Null, GenericRecord>>(producer);
-        //    var typedProducer = (KafkaProducer<Null, GenericRecord>)producer;
-        //    Assert.NotNull(typedProducer.ValueSerializer);
-        //    Assert.IsType<SyncOverAsyncSerializer<GenericRecord>>(typedProducer.ValueSerializer);
-        //}
+            Assert.NotNull(producer);
+            Assert.IsType<KafkaProducer<Null, GenericRecord>>(producer);
+            var typedProducer = (KafkaProducer<Null, GenericRecord>)producer;
+            Assert.NotNull(typedProducer.ValueSerializer);
+            Assert.IsType<SyncOverAsyncSerializer<GenericRecord>>(typedProducer.ValueSerializer);
+        }
 
         [Fact]
         public void When_Value_Type_Is_Specific_Record_Should_Create_SpecificRecord_Listener()
