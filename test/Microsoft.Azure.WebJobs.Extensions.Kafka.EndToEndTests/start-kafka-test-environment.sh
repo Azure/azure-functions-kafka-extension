355b6e79fb7ed2ab7c45bef3dbe5c3029c558c91
Pranava Vedagnya Gaddam <prgaddam@microsoft.com>
2025-07-23 13:10:14 +0530

    increase endtoend test startup time

--- test/Microsoft.Azure.WebJobs.Extensions.Kafka.EndToEndTests/start-kafka-test-environment.sh
+++ test/Microsoft.Azure.WebJobs.Extensions.Kafka.EndToEndTests/start-kafka-test-environment.sh
@@ -9,4 +9,4 @@ docker-compose -f ./kafka-singlenode-compose.yaml  up --build -d
 
 # wait until kafka is ready to create topic
 # need to improve, adding a retry instead of a static sleep
-sleep 30
+sleep 180
