2b47e492df4b50ab9a96db9250f0d4ac074caaa4
Pranava Vedagnya Gaddam <prgaddam@microsoft.com>
2025-07-24 16:11:14 +0530

    remove unused directory

--- src/Microsoft.Azure.WebJobs.Extensions.Kafka/Serialization/SerializationHelper.cs
+++ src/Microsoft.Azure.WebJobs.Extensions.Kafka/Serialization/SerializationHelper.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Avro.Generic;
 using Avro.Specific;
 using Confluent.Kafka;
